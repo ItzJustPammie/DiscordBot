40aff06d71dff8b5e0aab0a2efe2c08308694750
ItzJustPammie <30531532+ItzJustPammie@users.noreply.github.com>
2018-10-12 14:57:14 +0200

    Updated ip.js

--- src/commands/ip.js
+++ src/commands/ip.js
@@ -1,1 +1,16 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+        .setDescription("WantedNetwork")
+        .setColor("#ff0000")
+        .setThumbnail(bicon)
+        .addField("Het IP van WantedNetwork is:", "play.wantednetwork.net");
+
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+    name:"ip"
+}
